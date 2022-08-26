5feb8dd1a97120a7e4db016ef69553d771537822
cymatic81 <100329552+cymatic81@users.noreply.github.com>
2022-08-26 10:43:42 +0930

    Add files via upload

--- libft/libft/ft_memset.c
+++ libft/libft/ft_memset.c
@@ -6,25 +6,23 @@
 /*   By: jchapman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 11:49:21 by jchapman          #+#    #+#             */
-/*   Updated: 2022/08/24 10:52:03 by jchapman         ###   ########.fr       */
+/*   Updated: 2022/08/26 10:36:00 by jchapman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	*ft_memset(void *str, int x, size_t length)
 {
-	char	*b;
-	char	nc;
-	int		i;
+	unsigned char	b;
+	int				i;
 
 	i = 0;
-	&b = (char) *(*str);
-	nc = (char) x;
+	b = (unsigned char) str;
 
 	while (i < length)
 	{
-		&b[i] = nc;
+		b[i] = x;
 		i++;
 	}
-	char *(*str) = *b;
+	return (str = b);
 }
 
